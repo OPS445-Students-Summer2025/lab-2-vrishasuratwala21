8f89639fce305e8a67859513cdd0518602438bdc
Vrisha Suratwala <vvsuratwala@myseneca.ca>
2025-05-14 18:42:15 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#Author: Vrisha Suratwala 
+#Author id: vvsuratwala@myseneca.ca
+#Date created: 2025/05/14
+
+timer = int(10)
+while timer > 0:
+	print(timer)
+	timer = timer - 1
+print("blast off!")
