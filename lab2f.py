c86d2839c3535130e5446c6c446553f4b3c21cd5
Vrisha Suratwala <vvsuratwala@myseneca.ca>
2025-05-14 18:46:19 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#Author: Vrisha Suratwala 
+#Author id: vvsuratwala@myseneca.ca
+#Date created: 2025/05/14
+
+import sys
+
+timer = int(sys.argv[1])
+while timer > 0:
+        print(timer)
+        timer = timer - 1
+print("blast off!")
